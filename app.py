6859a020b5c8a16b96c52631797bc094e3d22587
Ashish Ranjan Karn <ashish.ranjan@grabtaxi.com>
2022-01-11 17:45:20 +0800

    update

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, request, json, redirect, url_for
 from flask import render_template
 from imap_tools import MailBox
+from job_descriptions import jds
+import difflib
 
 app = Flask(__name__)
 app.debug = True
+
+
+buckets = [jd['jobid']+" "+jd['position']+" "+jd['keywords'] for jd in jds]
+print(buckets)
 
 
 
@@ -21,21 +27,31 @@ def signin():
         with MailBox('imap.gmail.com').login(username, password, 'INBOX') as mailbox:
             print("success")
             for i, msg in enumerate(mailbox.fetch(bulk=True, limit=20, reverse=True)):
-                email = {'attachments':[], 'from':msg.from_, 'sub':msg.subject}
+                email = {'attachments':[], 'from':msg.from_, 'sub':msg.subject, 'job':""}
                 email['id'] = i+1
                 emails.append(email)
                 for att in msg.attachments:
                     email['attachments'].append(att.filename)
+                email['job'] = classify(msg.subject, msg.text)
         return render_template('basic_table.html', title='Gmail Jobs classifier',
                            emails=emails)
 
 
     return json.dumps({'validation' : False})
 
+
+def classify(subject, text):
+    if ('job' in subject) | ('job' in text):
+        ll = [difflib.SequenceMatcher(a=subject.lower() +" "+ text.lower(), b=b.lower()).ratio() for b in buckets]
+        ind = ll.index(max(ll))
+        print(f"index: {ind}")
+        return jds[ind]['jobid']+" ("+jds[ind]['position']+")"
+    else: return ""
+
     
             
     
 if __name__ == '__main__':
-   app.run()
+   app.run(host='0.0.0.0', debug=True)
 
 
---
+++ job_descriptions.py
@@ -0,0 +1,6 @@
+jds = [
+    {'jobid':'J1', 'position':'Network Engineer', 'keywords':""},
+    {'jobid':'J2', 'position':'Hardware Engineer', 'keywords':"Experience 5 years minimum"},
+    {'jobid':'J3', 'position':'Software Engineer', 'keywords':"Automation"},
+    {'jobid':'J4', 'position':'Salesman', 'keywords':""},
+]
